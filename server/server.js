26a337200695fbd215d213fa5ecaaf9a5ed83b7f
Janghoon Lee <janghoon@minerva.kgi.edu>
2019-01-10 18:14:05 -0800

    Add location message

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -14,6 +16,20 @@ app.use(express.static(publicPath));
 
 io.on('connection', socket => {
 	console.log('New user connected');
+
+	socket.emit('newMessage', generateMessage('Admin', 'Welcome!'));
+
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+	socket.on('createMessage', (message, callback) => {
+		console.log('createMessage', generateMessage(message.from, message.text))
+		io.emit('newMessage', generateMessage(message.from, message.text));
+		callback('From the server');
+	});
+
+	socket.on('createLocationMessage', coords => {
+		io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+	})
 
 	socket.on('disconnect', () => {
 		console.log('User was disconnected');
